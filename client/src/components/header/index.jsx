f7dc91c47bb387e22539432b1f023e971d03e8dc
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-12 18:34:48 -0700

    Add Header Component

---
+++ client/src/components/header/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css";
+
+export default function Header() {
+  return (
+    <div className="styles.header">
+      <h3>JD MERN Blog App</h3>
+      <ul>
+        <li>Home</li>
+        <li>Add Blog</li>
+      </ul>
+    </div>
+  );
+}
