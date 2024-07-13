4f4ae9ba9a87d0dc5a283be766f47f7a19d2bd98
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-12 18:38:56 -0700

    Add Header Component

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.css";
 
 export default function Header() {
   return (
-    <div className="styles.header">
+    <div className={styles.header}>
       <h3>JD MERN Blog App</h3>
       <ul>
         <li>Home</li>
