476a88263d56200a775e285a72e1f143f1f2086d
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-12 17:24:53 -0700

    Feature Add a Blog Controller

--- server/controller/blog-controller.js
+++ server/controller/blog-controller.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Blog = require('../model/Blog')
+const mongoose = require("mongoose");
+const Blog = require("../model/Blog");
 
 //fetch a list of blogs
 //add a new blog
@@ -7,18 +7,45 @@ const Blog = require('../model/Blog')
 //update a blog
 //These are four controllers we need for the blog
 
-const fetchListOfBlogs = async(req,res) =>{
-    let blogList;
-    try{
-        blogList = await Blog.find();
-    }catch(e){
-        console.log(e)
-    }
+const fetchListOfBlogs = async (req, res) => {
+  let blogList;
+  try {
+    blogList = await Blog.find();
+  } catch (e) {
+    console.log(e);
+  }
 
-    if(!blogList){
-        //404 Error
-        return res.status(404).json({message: 'No Blogs Found'})
-    }
-    //200 Ok
-    return res.status(200).json(blogList)
-}
+  if (!blogList) {
+    //404 Error
+    return res.status(404).json({ message: "No Blogs Found" });
+  }
+  //200 Ok
+  return res.status(200).json(blogList);
+};
+
+const addNewBlog = async (req, res) => {
+  const { title, description } = req.body;
+  const currentDate = new Date();
+
+  const newlyCreatedBlog = new Blog({
+    title,
+    description,
+    date: currentDate,
+  });
+  try {
+    await new newlyCreatedBlog.save();
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newlyCreatedBlog.save(session);
+    session.commitTransaction();
+  } catch (e) {
+    return res.send(500).json({ message: e });
+  }
+
+  return res.status(200).json({ newlyCreatedBlog });
+};
