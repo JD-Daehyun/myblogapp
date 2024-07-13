8bff89aa698afc742ff3ba37e55e54d7747b1474
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-12 19:00:44 -0700

    Add formData Global Context to AddNewBlog page

--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,10 +1,43 @@
+import { useContext } from "react";
+import styles from "./styles.module.css";
+import { GlobalContext } from "../../context";
+
+export default function AddNewBlog() {
+  const { formData, setFormData } = useContext(GlobalContext);
 
 
-
-
-export default function AddNewBlog(){
-
-    return <div>
-        <h1>Add A Blog</h1>
+  console.log(formData);
+  return (
+    <div className={styles.wrapper}>
+      <h1>Add A Blog</h1>
+      <div className={styles.formWrapper}>
+        <input
+          name="input"
+          placeholder="Enter Blog Title"
+          id="title"
+          type="text"
+          value={formData.title}
+          onChange={(event) =>
+            setFormData({
+              ...formData,
+              title: event.target.value,
+            })
+          }
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Blog Description"
+          id="description"
+          value={formData.description}
+          onChange={(event) =>
+            setFormData({
+              ...formData,
+              description: event.target.value,
+            })
+          }
+        />
+        <button >Add New Blog</button>
+      </div>
     </div>
+  );
 }
